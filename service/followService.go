5b91b015eba6da03840215b33fc4ce07173fc15f
董正宇 <dzy20010816@163.com>
2023-01-27 17:55:33 +0800

    fix: 删除omitempty

--- service/followService.go
+++ service/followService.go
@@ -1,11 +1,11 @@
 package service
 
 type User struct {
-	Id            int64  `json:"id,omitempty"`
-	Name          string `json:"name,omitempty"`
-	FollowCount   int64  `json:"follow_count,omitempty"`
-	FollowerCount int64  `json:"follower_count,omitempty"`
-	IsFollow      bool   `json:"is_follow,omitempty"`
+	Id            int64  `json:"id"`
+	Name          string `json:"name"`
+	FollowCount   int64  `json:"follow_count"`
+	FollowerCount int64  `json:"follower_count"`
+	IsFollow      bool   `json:"is_follow"`
 }
 
 type FollowService interface {
