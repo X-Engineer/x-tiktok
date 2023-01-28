b29c98ae55e30ff5c73a9952ee45077aa0e69f10
coder-zc <51215903107@stu.ecnu.edu.cn>
2023-01-28 15:54:33 +0800

    fix:合并User多定义冲突

--- service/userService.go
+++ service/userService.go
@@ -19,9 +19,9 @@ type UserService interface {
 }
 
 type User struct {
-	Id             int64  `json:"id,omitempty"`
-	Name           string `json:"name,omitempty"`
-	FollowCount    int64  `json:"follow_count,omitempty"`
-	FollowerCount  int64  `json:"follower_count,omitempty"`
-	IsFollow       bool   `json:"is_follow,omitempty"`
+	Id            int64  `json:"id"`
+	Name          string `json:"name"`
+	FollowCount   int64  `json:"follow_count"`
+	FollowerCount int64  `json:"follower_count"`
+	IsFollow      bool   `json:"is_follow"`
 }
