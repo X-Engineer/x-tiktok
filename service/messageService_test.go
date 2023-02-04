5a861095fddf8a4e8ab184a50394a060c7b35727
nzcer <ningzhicheng1215@qq.com>
2023-02-05 00:58:14 +0800

    feature: 初步完成消息模块
    聊天记录接口还需要debug，详情介绍情况 https://github.com/X-Engineer/x-tiktok/issues/12

---
+++ service/messageService_test.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestMessageServiceImpl_SendMessage(t *testing.T) {
+	err := messageServiceImpl.SendMessage(2, 8, "2 号用户发送消息给 8 号用户", 1)
+	if err == nil {
+		log.Println("SendMessage Service 正常")
+	}
+}
+
+func TestMessageServiceImpl_MessageChat(t *testing.T) {
+	chat, _ := messageServiceImpl.MessageChat(8, 2)
+	for _, msg := range chat {
+		log.Println(fmt.Sprintf("%+v", msg))
+	}
+}
+
+func TestMessageServiceImpl_LatestMessage(t *testing.T) {
+	message, _ := messageServiceImpl.LatestMessage(7, 1)
+	log.Println(fmt.Sprintf("%+v", message))
+}
