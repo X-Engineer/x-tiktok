1c627c625090c75ad76d7db20050c6bedb548f72
逝不等琴生 <39022409+zhicheng-ning@users.noreply.github.com>
2023-02-05 11:45:06 +0800

    feature: 初步完成消息模块 (#25)
    聊天记录接口还需要debug，详情介绍情况 https://github.com/X-Engineer/x-tiktok/issues/12

---
+++ dao/messageDao.go
@@ -0,0 +1,76 @@
+package dao
+
+import (
+	"log"
+	"time"
+	"x-tiktok/config"
+)
+
+type Message struct {
+	Id         int64     `json:"id" gorm:"user_id"`
+	UserId     int64     `json:"user_id" gorm:"user_id"`
+	ReceiverId int64     `json:"receiver_id" gorm:"receiver_id"`
+	ActionType int64     `json:"action_type" gorm:"action_type"`
+	MsgContent string    `json:"msg_content" gorm:"msg_content"`
+	CreatedAt  time.Time `json:"created_at" gorm:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at" gorm:"updated_at"`
+}
+
+// APIMessage 返回提取的消息（仅需 Message 的部分字段）
+type APIMessage struct {
+	Id         int64     `json:"id"`
+	MsgContent int64     `json:"content" gorm:"msg_content"`
+	CreatedAt  time.Time `json:"create_time"`
+}
+
+func (Message) TableName() string {
+	return "message"
+}
+
+func SaveMessage(msg Message) error {
+	result := Db.Save(&msg)
+	if result.Error != nil {
+		log.Println("数据库保存消息失败！", result.Error)
+		return result.Error
+	}
+	return nil
+}
+
+// SendMessage fromUserId 发送消息 content 给 toUserId
+func SendMessage(fromUserId int64, toUserId int64, content string, actionType int64) error {
+	var message Message
+	message.UserId = fromUserId
+	message.ReceiverId = toUserId
+	message.ActionType = actionType
+	message.MsgContent = content
+	message.CreatedAt = time.Now()
+	message.UpdatedAt = time.Now()
+	return SaveMessage(message)
+}
+
+// MessageChat 当前登录用户和其他指定用户的聊天记录
+func MessageChat(loginUserId int64, targetUserId int64) ([]Message, error) {
+	messages := make([]Message, 0, config.VIDEO_NUM_PER_REFRESH)
+	result := Db.Where(&Message{UserId: loginUserId, ReceiverId: targetUserId}).
+		Or(&Message{UserId: targetUserId, ReceiverId: loginUserId}).
+		Order("created_at asc").
+		Find(&messages)
+	if result.Error != nil {
+		log.Println("获取聊天记录失败！")
+		return nil, result.Error
+	}
+	return messages, nil
+}
+
+// LatestMessage 返回 loginUserId 和 targetUserId 最近的一条聊天记录
+func LatestMessage(loginUserId int64, targetUserId int64) (Message, error) {
+	var message Message
+	result := Db.Where(&Message{UserId: loginUserId, ReceiverId: targetUserId}).
+		Or(&Message{UserId: targetUserId, ReceiverId: loginUserId}).
+		Order("created_at desc").Limit(1).Take(&message)
+	if result.Error != nil {
+		log.Println("获取最近一条聊天记录失败！")
+		return Message{}, result.Error
+	}
+	return message, nil
+}
---
+++ service/messageServiceImpl.go
@@ -0,0 +1,91 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+	"x-tiktok/config"
+	"x-tiktok/dao"
+)
+
+type MessageServiceImpl struct {
+}
+
+var (
+	messageServiceImpl *MessageServiceImpl
+	messageServiceOnce sync.Once
+)
+
+// GetMessageServiceInstance Go 单例模式
+func GetMessageServiceInstance() *MessageServiceImpl {
+	messageServiceOnce.Do(func() {
+		messageServiceImpl = &MessageServiceImpl{}
+	})
+	return messageServiceImpl
+}
+
+func (messageService *MessageServiceImpl) SendMessage(fromUserId int64, toUserId int64, content string, actionType int64) error {
+	var err error
+	switch actionType {
+	// actionType = 1 发送消息
+	case 1:
+		err = dao.SendMessage(fromUserId, toUserId, content, actionType)
+	default:
+		log.Println(fmt.Sprintf("未定义 actionType=%d", actionType))
+		return errors.New(fmt.Sprintf("未定义 actionType=%d", actionType))
+	}
+	return err
+}
+
+func (messageService *MessageServiceImpl) MessageChat(loginUserId int64, targetUserId int64) ([]Message, error) {
+	messages := make([]Message, 0, config.VIDEO_INIT_NUM_PER_AUTHOR)
+	plainMessages, err := dao.MessageChat(loginUserId, targetUserId)
+	if err != nil {
+		log.Println("MessageChat Service:", err)
+		return nil, err
+	}
+	err = messageService.getRespMessage(&messages, &plainMessages)
+	if err != nil {
+		log.Println("getRespMessage:", err)
+		return nil, err
+	}
+	return messages, nil
+}
+
+func (messageService *MessageServiceImpl) LatestMessage(loginUserId int64, targetUserId int64) (LatestMessage, error) {
+	plainMessage, err := dao.LatestMessage(loginUserId, targetUserId)
+	if err != nil {
+		log.Println("LatestMessage Service:", err)
+		return LatestMessage{}, err
+	}
+	var latestMessage LatestMessage
+	latestMessage.message = plainMessage.MsgContent
+	if plainMessage.UserId == loginUserId {
+		// 最新一条消息是当前登录用户发送的
+		latestMessage.msgType = 1
+	} else {
+		// 最新一条消息是当前好友发送的
+		latestMessage.msgType = 0
+	}
+	return latestMessage, nil
+}
+
+// 返回 message list 接口所需的 Message 结构体
+func (messageService *MessageServiceImpl) getRespMessage(messages *[]Message, plainMessages *[]dao.Message) error {
+	for _, tmpMessage := range *plainMessages {
+		var message Message
+		messageService.combineMessage(&message, &tmpMessage)
+		*messages = append(*messages, message)
+	}
+	return nil
+}
+
+func (messageService *MessageServiceImpl) combineMessage(message *Message, plainMessage *dao.Message) error {
+	message.Id = plainMessage.Id
+	message.UserId = plainMessage.UserId
+	message.ReceiverId = plainMessage.ReceiverId
+	message.MsgContent = plainMessage.MsgContent
+	message.CreatedAt = plainMessage.CreatedAt.Format(config.GO_STARTER_TIME)
+	return nil
+}
